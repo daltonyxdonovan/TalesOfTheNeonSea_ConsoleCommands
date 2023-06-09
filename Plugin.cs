b16b4123bc39f6b3caff3d5fdad257c25fae47e5
daltonyxdonovan <daltondonovan@yahoo.com>
2023-06-09 17:03:54 -0500

    initial commit add achievement unlocker

---
+++ Plugin.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using UnityEngine;
+
+
+namespace TalesOfTheNeonSea_ConsoleCommands
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        private void Update()
+        {
+            // Plugin update logic
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                for (int i = 0; i < 69; i++)
+                {
+                    AchievementManager.Instance.UnlockAchievement(i);
+                }
+            }
+
+
+        }
+    }
+}
---
+++ obj/Debug/net46/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace TalesOfTheNeonSea_ConsoleCommands
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "TalesOfTheNeonSea_ConsoleCommands";
+        public const string PLUGIN_NAME = "TalesOfTheNeonSea_ConsoleCommands";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
